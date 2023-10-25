77292e29e37a09a0071b7021efdcceec7fa22a97
adaz008 <adi.szigeti@gmail.com>
2023-10-26 01:08:30 +0200

    fix bumper, hood camera and add inside camera

--- Assets/Scripts/Car/CameraController.cs
+++ Assets/Scripts/Car/CameraController.cs
@@ -1,103 +1,95 @@
+using System.ComponentModel;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public Transform player;
     private Rigidbody playerRB;
+    private CarMovement carMovement;
+
     [Header("Distance")]
     [SerializeField] private float Distance_Offset_Close;
     [SerializeField] private float Distance_Offset_Far;
-    [Header("Height")]
-    [SerializeField] private float Height_Offset_Close;
-    [SerializeField] private float Height_Offset_Far;
-    [SerializeField] private float speed;
-    private CarMovement carMovement;
 
-    // Start is called before the first frame update
-    void Start()
+    [Header("Height")]
+    [SerializeField] private float Height_Offset;
+
+    [Header("Camera change speed")]
+    [SerializeField] private float speed;
+
+    [Header("OffsetVectors")]
+    [SerializeField] private Vector3 hood;
+    [SerializeField] private Vector3 bumper;
+    [SerializeField] private Vector3 inside;
+
+    private void Start()
     {
         playerRB = player.GetComponent<Rigidbody>();
         carMovement = player.GetComponent<CarMovement>();
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    private void LateUpdate()
     {
         Vector3 playerForward = (-playerRB.velocity - player.transform.forward).normalized;
 
-        Vector3 hood = new Vector3(-1.469f, 0.393f, -0.008f);
-        Vector3 bumper = new Vector3(-2.4f, -0.02f, -0.008f);
-        Vector3 back = new Vector3(-8f, 2f, -0.008f);
+        if (UserSettings.Instance.Camera == "Hood")
+            SetCameraPosition(hood);
+        else if (UserSettings.Instance.Camera == "Bumper")
+            SetCameraPosition(bumper);
+        else if (UserSettings.Instance.Camera == "Inside")
+            SetCameraPosition(inside);
 
-        if (carMovement.LookBack)
+        float distanceOffSet = UserSettings.Instance.Camera == "Close" ? Distance_Offset_Close : Distance_Offset_Far;
+
+        if (carMovement.LookBack && (UserSettings.Instance.Camera == "Close" || UserSettings.Instance.Camera == "Far"))
         {
-            transform.position = player.position - playerForward * 8f;
-            if (UserSettings.Instance.Camera == "Close" && carMovement.GasInput >= 0)
-                transform.position = player.position - playerForward * Distance_Offset_Close + Vector3.up * Height_Offset_Close;
-            if (UserSettings.Instance.Camera == "Close" && carMovement.GasInput < 0)
-                transform.position = player.position + playerForward * Distance_Offset_Close + Vector3.up * Height_Offset_Close;
-            if (UserSettings.Instance.Camera == "Far" && carMovement.GasInput >= 0)
-                transform.position = player.position - playerForward * Distance_Offset_Far + Vector3.up * Height_Offset_Far;
-            if (UserSettings.Instance.Camera == "Far" && carMovement.GasInput < 0)
-                transform.position = player.position + playerForward * Distance_Offset_Far + Vector3.up * Height_Offset_Far;
-            transform.LookAt(player);
+            Vector3 forward = (playerForward.x > 0 && playerForward.z > 0) ? -playerForward : playerForward;
+            SetCameraPositionFarClose(forward, distanceOffSet);
         }
-        else
+        else if (UserSettings.Instance.Camera == "Close" || UserSettings.Instance.Camera == "Far")
         {
-            if (UserSettings.Instance.Camera == "Hood")
-                transform.position = player.position + hood;
-            else if (UserSettings.Instance.Camera == "Bumper")
-                transform.position = player.position + bumper;
-            else if (UserSettings.Instance.Camera == "Far" && !UserSettings.Instance.ChangeCameraReverse)
-            {
-                Vector3 targetPosition = player.position + Height_Offset_Far * Vector3.up - player.forward * Distance_Offset_Far;
-
-                if (Vector3.Distance(transform.position, targetPosition) > 1)
-                    transform.position = targetPosition;
-                else
-                    transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
-
-                transform.LookAt(player);
-            }
-            else if (UserSettings.Instance.Camera == "Close" && !UserSettings.Instance.ChangeCameraReverse)
-            {
-                Vector3 targetPosition = player.position + Height_Offset_Close * Vector3.up - player.forward * Distance_Offset_Close;
-
-                if (Vector3.Distance(transform.position, targetPosition) > 1)
-                    transform.position = targetPosition;
-                else
-                    transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
-
-                transform.LookAt(player);
-            }
-            else if (UserSettings.Instance.Camera == "Far" && UserSettings.Instance.ChangeCameraReverse)
-            {
-                transform.position = Vector3.Lerp(transform.position,
-                player.position + Height_Offset_Far * Vector3.up
-                + playerForward * Distance_Offset_Far,
-                speed * Time.deltaTime);
-
-                transform.LookAt(player);
-            }
-            else if (UserSettings.Instance.Camera == "Close" && UserSettings.Instance.ChangeCameraReverse)
-            {
-                transform.position = Vector3.Lerp(transform.position,
-                player.position + Height_Offset_Close * Vector3.up
-                + playerForward * Distance_Offset_Close,
-                speed * Time.deltaTime);
-
-                transform.LookAt(player);
-            }
+            SetCameraPositionFarClose(playerForward, distanceOffSet);
         }
 
-        if (UserSettings.Instance.Camera == "Hood" && PauseMenu.GameIsPaused)
-            transform.position += new Vector3(0f, 1f, 0f);
+        HandleCameraFieldOfView();
+    }
 
-        if (NitroEffect.boosting && NitroEffect.nitroValue > 0)
-            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, 100, Time.deltaTime);
+    private void SetCameraPosition(Vector3 position)
+    {
+        transform.position = player.TransformPoint(position);
+    }
+
+    private void SetCameraPositionFarClose(Vector3 playerForward, float distance)
+    {
+        Vector3 targetPosition = player.position + Height_Offset * Vector3.up - player.forward * distance;
+
+        if (UserSettings.Instance.ChangeCameraReverse)
+            transform.position = Vector3.Lerp(transform.position,
+                player.position + Height_Offset * Vector3.up + playerForward * distance,
+                speed * Time.deltaTime);
         else
-            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, 60, Time.deltaTime);
+        {
+            if (Vector3.Distance(transform.position, targetPosition) > 1)
+                transform.position = targetPosition;
+            else
+                transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
+        }
 
+        transform.LookAt(player);
+    }
 
+    private void HandleCameraFieldOfView()
+    {
+        if ((UserSettings.Instance.Camera == "Hood" || UserSettings.Instance.Camera == "Bumper" || UserSettings.Instance.Camera == "Inside") && PauseMenu.GameIsPaused)
+        {
+            transform.position = player.position + new Vector3(0f, 3f, 0f);
+        }
+
+        if (UserSettings.Instance.Camera == "Close" || UserSettings.Instance.Camera == "Far")
+        {
+            float fieldViewValue = (NitroEffect.boosting && NitroEffect.nitroValue > 0) ? 100f : 60f;
+
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, fieldViewValue, Time.deltaTime);
+        }
     }
 }
